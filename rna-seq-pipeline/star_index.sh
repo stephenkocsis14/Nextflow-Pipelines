dce101d2ecfabaf22c1958f4aae583b8d224bb48
Stephen "Christian" Kocsis <89398186+stephenkocsis14@users.noreply.github.com>
2024-11-07 13:07:48 -0500

    Delete rna-seq-pipeline directory

--- rna-seq-pipeline/star_index.sh
+++
@@ -1,2 +0,0 @@
-# Generate the STAR genome index given any reference fasta and gtf
-STAR --runMode genomeGenerate --genomeDir genome/STAR_index --genomeFastaFiles /path/to/genome.fasta --sjdbGTFfile /path/to/annotation.gtf --runThreadN 4
